b56d69e22bbb943e02acfe0ca119ec4de42cc59b
Dafin Aziz <Dafin@users.noreply.github.com>
2022-08-08 00:01:01 +0100

    Start, Stop, Reset Buttons

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -1,7 +1,18 @@
 import React, { useState, useEffect } from "react";
 
-const Stopwatch = () => { 
-  //...
+const Stopwatch = () => {
+    // TODO output stopwatch readout
+    //
+    return (
+        <div className="stopwatch">
+            <div className="buttons">
+                <button>Start</button>
+                <button>Stop</button>
+                <button>Reset</button>
+            </div>
+        </div>
+    );
+
 };
 
 export default Stopwatch;
