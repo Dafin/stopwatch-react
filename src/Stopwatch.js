f601216b9efb8a5317dabf6e531c6f40550fc2a8
Dafin Aziz <Dafin@users.noreply.github.com>
2022-08-08 22:41:15 +0100

    Implementing useState-  declare variable for ticking state
    React hook:  Declaring variables for the ticking (running of the stopwatch) and adding button event handlers to STOP and START buttons

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -1,18 +1,19 @@
 import React, { useState, useEffect } from "react";
 
 const Stopwatch = () => {
+
+    const [ticking, setTicking] = React.useState(false);
+
     // TODO output stopwatch readout
-    //
     return (
         <div className="stopwatch">
-        <h1>ReactJS Stopwatch</h1>
+            <h1>ReactJS Stopwatch</h1>
             <div className="readout">
                 00:00:00
             </div>
             <div className="buttons">
-                <button id="start">Start</button>
-                <button id="stop">Stop</button>
-                <button id="reset">Reset</button>
+                <button id="start" onClick={() => setTicking(true)}>Start</button>
+                <button id="reset" >Reset</button>
             </div>
         </div>
     );
