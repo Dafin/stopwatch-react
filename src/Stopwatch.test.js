1e8583ea046c1b4f2bd0e25856c25455c1b910e9
Dafin Aziz <Dafin@users.noreply.github.com>
2022-08-14 11:43:20 +0100

    Test Suite created
    From project spec

--- src/Stopwatch.test.js
+++ src/Stopwatch.test.js
@@ -1,37 +1,21 @@
 import { Stopwatch } from './Stopwatch'
 import { render, screen, fireEvent } from '@testing-library/react';
 
-describe("Stopwatch", () => {
+describe("<StopWatch />", () => {
     /// https://testing-library.com/docs/react-testing-library/example-intro
     it('Stopwatch should appear in the document .', () => {
         render(<Stopwatch />)
         const result = screen.getByTestId('stop-watch');
         expect(result).toBeInTheDocument();
     });
-
-    it.todo('The stopwatch SHOULD have the ability to be started and stopped.');
+    it.todo('Check presence of start button');
+    it.todo('Check presence of stop button');
+    it.todo("The stopwatch SHOULD have the ability to be started and stopped");
+    it.todo('Check presence of reset button');
+    it.todo('Reset button zero\'s the time.');
+    it.todo('The stopwatch SHOULD display the number of seconds')
+    it.todo('The stopwatch renders the seconds.');
+    it.todo('"The stopwatch COULD display the time in any recognisable timeformat (i.e hh:mm:ss)."');
+    it.todo('The stopwatch displays in the hh:mm:ssformat');
+    it.todo('Detailed styling IS NOT important - show your own creativity.');
 });
-
-
-
-
-
-// test.skip('The stopwatch SHOULD have the ability to be started and stopped.', () => {
-//     // TODO Check; button presence, functionality, multiple clicks
-// });
-
-// test.skip('The stopwatch SHOULD display the number of seconds.', () => {
-//     // TODO
-// });
-
-// test.skip('The stopwatch COULD have the ability to be reset without refreshing the page', () => {
-//     // TODO
-// });
-
-// test.skip('The stopwatch COULD display the time in any recognisable timeformat (i.e hh:mm:ss)', () => {
-//     // TODO
-// });
-
-// test.skip('Detailed styling IS NOT important - show your own creativity.', () => {
-//     // TODO 
-// });
