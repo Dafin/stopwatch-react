b19e069885693f720334a587d8d73f302d4064cc
Dafin Aziz <Dafin@users.noreply.github.com>
2022-08-15 17:17:02 +0100

    Core Implementation
    - Exported Stopwatch to be available for testing
    - useEffect implemented (takes ticking as an argument)- if ticking state is true, setInterval of 1(ms)
    - Digit rendering: separately rendering individual time segment 'components' , adding a ternary operator to add a zero to display if evaluated as zero
    -rendering time display in readout panel panel using template literals
    -Adding data-testid to elements for testing

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -1,25 +1,68 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
-const Stopwatch = () => {
+export const Stopwatch = () => {
 
-    const [ticking, setTicking] = React.useState(false);
+    const [ticking, setTicking] = useState(false);
     const [timeDisplay, setTimeDisplay] = useState(0);
 
+    useEffect(() => {
+        let interval;
+        if (ticking) {
+            interval = setInterval(() => {
+                setTimeDisplay((prevTimeDisplay) => prevTimeDisplay + 1);
+            }, 1);
+        } else if (!ticking) {
+            clearInterval(interval);
+        }
+        return () => clearInterval(interval);
+    }, [ticking]);
+
+    const getHours = (time) => {
+       const result  = ("0" + Math.floor((time / 60000) % 60)).slice(-2);
+       return result === "00" ? "00" : result;
+    } 
+    const getMinutes = (time) => {
+        const result  = ("0" + Math.floor((time / 1000) % 60)).slice(-2);
+        return result === "00" ? "00" : result;
+     } 
+     const getSeconds = (time) => {
+        const result  = ("0" + Math.floor((time / 10) % 100)).slice(-2);
+        return result === "00" ? "00" : result;
+     } 
+
+    // TODO keyboard interaction
+    // const onKeyboardInteraction = (keyboardEvent) => {
+    //     switch (keyboardEvent.code) {
+    //         case 'Enter':
+    //             setTicking(true)
+    //             break
+    //         case 'Space':
+    //             setTicking(false);
+    //             break
+    //         case 'Escape':
+    //             setTimeDisplay(0)
+    //             break
+    //         default:
+    //             break
+    //     }
+    // }
+
     return (
-        <div className="stopwatch">
+        <div className="stopwatch" data-testid="stop-watch">
             <h1>ReactJS Stopwatch</h1>
-            <div className="readout">
-                {"0" + timeDisplay}:{"0" + timeDisplay}:{"0" + timeDisplay}
+            <div 
+                className="readout"
+                data-testid="readout-panel"
+                >
+                {getHours(timeDisplay)}:{getMinutes(timeDisplay)}:{getSeconds(timeDisplay)}
             </div>
             <div className="buttons">
-                <button id="start" onClick={() => setTicking(true)}>Start</button>
+                <button id="start" data-testid="start-button" onClick={() => setTicking(true)}>Start</button>
                 <button id="stop" onClick={() => setTicking(false)}>Stop</button>
                 <button id="reset" onClick={() => setTimeDisplay(0)} >Reset</button>
-
             </div>
         </div>
     );
 
 };
 
-export default Stopwatch;
